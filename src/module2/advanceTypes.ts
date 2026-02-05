60acd11598c5e58d21561faef2e002abb787a933
khairulanam23 <mka.rifat.24@gmail.com>
2026-02-05 10:30:32 +0600

    new ts practices

---
+++ src/module2/advanceTypes.ts
@@ -0,0 +1,112 @@
+{
+  //type assertion
+
+  let anything: any;
+  anything = "Khairul anam";
+  (anything as string).length;
+
+  const kgtoGm = (value: string | number): string | number | undefined => {
+    if (typeof value === "string") {
+      const convertedValue = parseFloat(value);
+      return convertedValue;
+    }
+    if (typeof value === "number") {
+      return value;
+    }
+  };
+
+  const result1 = kgtoGm(1000) as number;
+  const result2 = kgtoGm("1000") as string;
+
+  type CustomError = {
+    message: string;
+  };
+  try {
+  } catch (error) {
+    console.log((error as CustomError).message);
+  }
+}
+
+{
+  //interface
+
+  type User1 = {
+    name: string;
+    age: number;
+  };
+  //We can use premitive only using type aliass
+
+  type UserWithRole1 = User1 & { role: string };
+  const user3: UserWithRole1 = {
+    name: "khairul",
+    age: 24,
+    role: "manager",
+  };
+
+  interface User2 {
+    name: string;
+    age: number;
+  }
+
+  interface UserWithRole2 extends User2 {
+    role: string;
+  }
+
+  const user4: UserWithRole2 = {
+    name: "khairul",
+    age: 24,
+    role: "manager",
+  };
+
+  const user1: User1 = {
+    name: "Persian",
+    age: 100,
+  };
+
+  type Roll1 = number[];
+
+  interface Roll2 {
+    [index: number]: number;
+  }
+
+  const rollNumber1: Roll2 = [1, 2, 3, 4];
+
+  type Add1 = (num1: number, num2: number) => number;
+
+  interface Add2 {
+    (num1: number, num2: number): number;
+  }
+
+  const add: Add2 = (num1, num2) => num1 + num2;
+}
+
+{
+  //generic type
+  // const rollNumber : number [] = [3,5,8];
+  const rollNumber: Array<number> = [3, 5, 8];
+
+  // const mentors : string [] = ["Mr X", "Mr. Y", "Mr. Z"]
+  const mentors: Array<string> = ["Mr X", "Mr. Y", "Mr. Z"];
+
+  //type GenericArray = Array<boolean>
+  type GenericArray<T> = Array<T>;
+  const boolArray: GenericArray<boolean> = [true, false, true];
+
+  const user: GenericArray<{ name: string; age: number }> = [
+    {
+      name: "Khairul Anam",
+      age: 24,
+    },
+    {
+      name: " Rintu",
+      age: 23,
+    },
+  ];
+
+  //generic tuple 
+  type GenericTuple <X, Y> = [X, Y]
+
+  const manush : GenericTuple<string, string> = ["Mr. X ", "Mrs. Y"];
+
+  const UserWithId : GenericTuple<number, {name:string, email:string}> = [1234, {name : "persian", email: "persi@gmail.com"}]
+}
