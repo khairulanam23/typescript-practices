d332e8b48e494bd4f83df753581363ffce8a32ec
khairulanam23 <mka.rifat.24@gmail.com>
2026-02-05 10:53:44 +0600

    generic ts

--- src/module2/advanceTypes.ts
+++ src/module2/advanceTypes.ts
@@ -103,10 +103,110 @@
     },
   ];
 
-  //generic tuple 
-  type GenericTuple <X, Y> = [X, Y]
+  //generic tuple
+  type GenericTuple<X, Y> = [X, Y];
 
-  const manush : GenericTuple<string, string> = ["Mr. X ", "Mrs. Y"];
+  const manush: GenericTuple<string, string> = ["Mr. X ", "Mrs. Y"];
 
-  const UserWithId : GenericTuple<number, {name:string, email:string}> = [1234, {name : "persian", email: "persi@gmail.com"}]
+  const UserWithId: GenericTuple<number, { name: string; email: string }> = [
+    1234,
+    { name: "persian", email: "persi@gmail.com" },
+  ];
+}
+
+{
+  //generic type using Interface
+  interface Developer<T, X = null> {
+    name: string;
+    computer: {
+      brand: string;
+      model: string;
+      releaseYear: number;
+    };
+    smartWatch: T;
+    bike?: X;
+  }
+
+  const poorDevelopr: Developer<{
+    brand: string;
+    model: string;
+    display: string;
+  }> = {
+    name: "Khairul Anam",
+    computer: {
+      brand: "Asus",
+      model: "X-255UR",
+      releaseYear: 2013,
+    },
+    smartWatch: {
+      brand: " Emilab",
+      model: "kw66",
+      display: "Oled",
+    },
+  };
+
+  const richDevelopr: Developer<
+    {
+      brand: string;
+      model: string;
+      heartTrack: boolean;
+      sleepTrack: boolean;
+    },
+    null
+  > = {
+    name: "Ishrat Rintu",
+    computer: {
+      brand: "Hp",
+      model: "X-265UR",
+      releaseYear: 2023,
+    },
+    smartWatch: {
+      brand: " Apple Watch",
+      model: "Pro 4",
+      heartTrack: true,
+      sleepTrack: true,
+    },
+  };
+}
+
+{
+  //functions with generic
+
+  const createArray = (param: string): string[] => {
+    return [param];
+  };
+
+  const createArrayWithGeneric = <T>(param: T): T[] => {
+    return [param];
+  };
+
+  const res1 = createArray("Bangladesh");
+  const resGeneric = createArrayWithGeneric<string>("Bangladesh");
+  const resGeneric2 = createArrayWithGeneric<{
+    id: number;
+    name: string;
+  }>({
+    id: 24,
+    name: "Ishrat rintu",
+  });
+
+
+  const addCourseToStudent =<T> (student : T) =>{
+    const course = "Web Development"
+
+    return {
+        ...student,
+        course
+    }
+  }
+  const student1 = addCourseToStudent ({
+    name : "Mr. X",
+    email: "mrx@gmail.com",
+    devType: "Developer"
+  })
+  const student2 = addCourseToStudent ({
+    name : "Mr. Y",
+    email: "mry@gmail.com",
+    hasWatch : true,
+  })
 }
