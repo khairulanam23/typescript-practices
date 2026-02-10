d8cf87203f6d8b25b68719e556869eec180e3eec
khairulanam23 <mka.rifat.24@gmail.com>
2026-02-10 07:41:32 +0600

    Module 2 completed

--- src/module2/advanceTypes.ts
+++ src/module2/advanceTypes.ts
@@ -190,23 +190,155 @@
     name: "Ishrat rintu",
   });
 
-
-  const addCourseToStudent =<T> (student : T) =>{
-    const course = "Web Development"
+  const addCourseToStudent = <T>(student: T) => {
+    const course = "Web Development";
 
     return {
-        ...student,
-        course
-    }
-  }
-  const student1 = addCourseToStudent ({
-    name : "Mr. X",
+      ...student,
+      course,
+    };
+  };
+  const student1 = addCourseToStudent({
+    name: "Mr. X",
     email: "mrx@gmail.com",
-    devType: "Developer"
-  })
-  const student2 = addCourseToStudent ({
-    name : "Mr. Y",
+    devType: "Developer",
+  });
+  const student2 = addCourseToStudent({
+    name: "Mr. Y",
     email: "mry@gmail.com",
-    hasWatch : true,
-  })
+    hasWatch: true,
+  });
+}
+
+{
+  //constraint
+  const addCourseToStudent = <T extends { name: string; email: string }>(
+    student: T,
+  ) => {
+    const course = "Web Development";
+
+    return {
+      ...student,
+      course,
+    };
+  };
+}
+
+{
+  type Vehicle = {
+    bike: string;
+    car: string;
+    ship: string;
+  };
+
+  type owner = keyof Vehicle;
+
+  const getPropertyValue = <X, Y extends keyof X>(obj: X, key: Y) => {
+    return obj[key];
+  };
+  const user = {
+    name: "Khairul Anam",
+    age: 24,
+    address: "Dhaka",
+  };
+
+  const result1 = getPropertyValue(user, "name");
+}
+
+{
+  //asynchronous typescript
+  //basic promise
+
+  const createPromise = (): Promise<string> => {
+    return new Promise<string>((resolve, reject) => {
+      const data: string = "something";
+      if (data) {
+        resolve(data);
+      } else {
+        reject("Failed to load data!");
+      }
+    });
+  };
+
+  //calling create promise function
+
+  const showData = async () => {
+    const data: string = await createPromise();
+    return data; //returning string type data
+    //console.log(data);
+  };
+
+  showData();
+}
+
+{
+  //conditional type
+
+  type Sheikh = {
+    bike: string;
+    car: string;
+    plane: string;
+    ship: string;
+  };
+
+  type CheckVehicle<T> = T extends Sheikh ? true : false;
+
+  type HasPlane = CheckVehicle<"plane">;
+}
+
+{
+  //mapped types
+  type AreaNumber = {
+    height: number;
+    width: number;
+  };
+
+  type AreaString<T> = {
+    [key in keyof T]: T[key];
+  };
+
+  const area1: AreaString<{ height: string; width: number }> = {
+    height: "100",
+    width: 50,
+  };
+}
+
+{
+  //utility types
+
+  //pick utility
+  type Person = {
+    name: string;
+    age: number;
+    email?: string;
+    contactNo: string;
+  };
+
+  type NameAge = Pick<Person, "name" | "age">;
+
+  //omit
+  type ContactInfo = Omit<Person, "name" | "age">;
+
+  //required
+  type PersonRequired = Required<Person>;
+
+  //partial
+  type PersonPartial = Partial<Person>;
+
+  //readonly
+  // const person1 : Person ={
+  //   name : "Khairul Anam",
+  //   age : 24,
+  //   contactNo : "01284"
+  // }
+  type PersonReadony = Readonly<Person>;
+
+  //record
+  type MyObj = Record<string, string>;
+
+  const obj1: MyObj = {
+    a: "aa",
+    b: "bb",
+    c: "cc",
+  };
 }
